369ce3d43141c7779ec77faec74668b8ccb84964
Vito <zhouwentao16@gmail.com>
2016-11-29 18:42:55 +0800

    适配器模式

--- design-patterns/src/main/java/com/vito16/design/adapter/Adapter.java
+++ design-patterns/src/main/java/com/vito16/design/adapter/Adapter.java
@@ -5,18 +5,16 @@
 
 /*
  * 修订记录:
- * muyu@yiji.com 2016-11-29 10:27 创建
+ * muyu@yiji.com 2016-11-29 14:52 创建
  *
  */
-package com.vito16.design.model;
+package com.vito16.design.adapter;
 
 /**
+ * 适配器类
+ *
  * @author 木鱼 muyu@yiji.com
  * @version 2016/11/29
  */
-public interface Product {
-
-    void use();
-
+public class Adapter {
 }
-
